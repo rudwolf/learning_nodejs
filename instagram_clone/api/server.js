ee6507af8afde088558f87198127bf296a078d94
Rodolfo Rodrigues <rudwolf@gmail.com>
2019-02-04 18:45:21 -0200

    Created Restful API Methods

--- instagram_clone/api/server.js
+++ instagram_clone/api/server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
-    mongodb = require('mongodb');
+    mongodb = require('mongodb'),
+    objectId = require('mongodb').ObjectID;
 
 var app = express();
 
@@ -11,9 +12,86 @@ var port = 8080;
 
 app.listen(port);
 
+var db = new mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}),{}
+);
+
 console.log('Server Listening on port 8080');
 
 app.get('/', function(req, res){
     var response = {msg:'Hello'};
     res.send(response);
-})
+});
+
+app.get('/api', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('posts', function (err, collection) {
+            collection.find().toArray(function (err, results) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+app.get('/api/:id', function (req, res) {
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('posts', function (err, collection) {
+            collection.find(objectId(req.params.id)).toArray(function (err, results) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+app.put('/api/:id', function (req, res) {
+    var postData = req.body;
+
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('posts', function (err, collection) {
+            collection.update(
+                { _id: objectId(req.params.id)},
+                {
+                    $set : { title: req.body.title }
+                },
+                {},
+                function (err, records) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(records);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+});
+
+app.delete('/api/:id', function (req, res) {
+    var postData = req.body;
+
+    db.open(function (err, mongoclient) {
+        mongoclient.collection('posts', function (err, collection) {
+            collection.remove(
+                { _id: objectId(req.params.id) },
+                function (err, records) {
+                    if (err) {
+                        res.json(err);
+                    } else {
+                        res.json(records);
+                    }
+                    mongoclient.close();
+                });
+        });
+    });
+});
